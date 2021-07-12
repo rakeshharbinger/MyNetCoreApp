1d6cace2181fdb4e6832246a711a8b7163736501
Rakesh Mishra <rakesh.mishra@epicbrokers.com>
2021-07-12 13:41:15 +0530

    grid datatable datasource binding complete

--- Models/User.cs
+++ Models/User.cs
@@ -8,16 +8,21 @@ namespace MyNetCoreApp.Models
 {
     public class User
     {
+        [Display(Order = 0)]
         public int UserId { get; set; }
         [Required(ErrorMessage = "Please enter first name")]
+        [Display(Order = 1)]
         public string FirstName { get; set; }
         [Required(ErrorMessage = "Please enter last name")]
+        [Display(Order = 2)]
         public string LastName { get; set; }
         
         [Required(ErrorMessage = "Please enter email address")]
+        [Display(Order = 3)]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         [DataType(DataType.Password)]
+        [Display(Order = 4)]
         public string Password { get; set; }
     }
 }
